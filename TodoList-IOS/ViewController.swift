55392b08584d6eaccdbf7b3ff6c516ded337682b
dewesh-007 <dewesh.sunuwar2121@gmail.com>
2020-05-13 13:31:39 +1000

    complete barButton installed

--- TodoList-IOS/ViewController.swift
+++ TodoList-IOS/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
+    
+    var itemArray = [
+        "Here is item one to do",
+        "here is item two to do",
+        "here is item three to do"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "TodoItemList")
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+    }
+    
 }
 
