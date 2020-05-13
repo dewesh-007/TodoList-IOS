6fb18faa904290ad3a18f04c7534d02ee0a25956
dewesh-007 <dewesh.sunuwar2121@gmail.com>
2020-05-13 12:44:42 +1000

    create TableViewController

---
+++ TodoList-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList-IOS
+//
+//  Created by Dewesh Sunuwar on 13/5/20.
+//  Copyright © 2020 Dewesh Sunuwar. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
